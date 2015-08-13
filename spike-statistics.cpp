dec58f72a4e7056e87364a62c3747efba3fb10d7
Ansel <aesthapus@gmail.com>
2015-08-13 16:24:30 -0400

    remove qtgui include

--- spike-statistics.cpp
+++ spike-statistics.cpp
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "spike-statistics.h"
-#include <QtGui>
 
 extern "C" Plugin::Object *createRTXIPlugin(void) {
 	return new SpikeStats();
